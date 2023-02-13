3132bf29450b55bfa96410c7068867927bbe6d11
Famidore <lokoroko456@gmail.com>
2023-02-14 00:20:47 +0100

    added functionality to gather links to texts

--- scrap.py
+++ scrap.py
@@ -17,15 +17,17 @@ class CitSong():
         self.soup = BeautifulSoup(pageContent, 'html5lib')
         title = self.soup.find_all(class_="title d-md-inline")
         artist = self.soup.find_all(class_="artist -inline")
+        link = self.soup.find_all(class_="title d-md-inline", href=True)
 
-        return (title, artist)
+        return (title, artist, link)
 
-    def storeData(self, title, artist):
-        for t, a in zip(title, artist):
+    def storeData(self, title, artist, link):
+        for t, a, l in zip(title, artist, link):
             with open (self.filePath, 'r+', encoding="utf-8") as f:
                 oldData = json.load(f)
                 oldData.append([{"artist": a.text.split('- ')[1],
-                                "songName": t.text}])
+                                "songName": t.text,
+                                "link": "https://teksciory.interia.pl" + l["href"]}])
                 f.seek(0)
                 json.dump(oldData, f, indent=2, ensure_ascii=False)
 
@@ -42,7 +44,7 @@ class CitSong():
 
     def __call__(self):
         for p in range(self.pageLimit):
-            self.storeData(self.getData(p)[0], self.getData(p)[1])
+            self.storeData(self.getData(p)[0], self.getData(p)[1], self.getData(p)[2])
         print("Finished!")
         # self.eraseData()
         
